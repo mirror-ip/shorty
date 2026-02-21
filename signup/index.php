7f1f878b981c296196ca6653ebc742ca80def1d3
mirror-ip <leduchoanghai@yahoo.com>
2026-02-21 20:03:05 +0700

    Update index.php

--- signup/index.php
+++ signup/index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Shorty: A simple URL shortener.
+ *
+ * @copyright Copyright (c) 2026, Hoàng Hải <leduchoanghai@yahoo.com>
+ * @license   MIT, http://www.opensource.org/licenses/mit-license.php
+ */
 
         header("Location: http://go.quangai.net/signup/sign-up.html", true, 301);
         exit();
